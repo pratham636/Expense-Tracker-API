0d5bc7014884058a0e01c08bca40857f0cd89b7f
pratham shah <92474926+pratham636@users.noreply.github.com>
2026-04-24 22:56:03 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base,sessionmaker
 
-URL_DATABASE="mysql+pymysql://root:password@localhost:3306/expenses_tracker_api"
+URL_DATABASE="mysql+pymysql://root:"your_password"@localhost:3306/expenses_tracker_api"
 engine=create_engine(URL_DATABASE)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base() 
